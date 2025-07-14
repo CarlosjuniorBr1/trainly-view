a7e66694af816831d737bc2fbbc00fb814e102a1
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2025-07-14 08:28:44 -0300

    feat: adicionando tabelas para listagem de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { MatTable, MatTableModule } from "@angular/material/table";
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTable, MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+  courses: Course[] = [
+    {_id: "1", name:"Introdução a machine learning", category: "IA"},
+    {_id: "2", name:"Processamento de linguagem Natural", category: "IA , NLP"}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
