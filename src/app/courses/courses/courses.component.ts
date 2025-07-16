5c69fc6e823cd7d81b2462e29fffb79116a1835e
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2025-07-16 11:35:16 -0300

    feat: chaamada HTTP GET

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,27 +4,29 @@ import { MatTable, MatTableModule } from "@angular/material/table";
 import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTable, MatTableModule, MatCardModule, MatToolbarModule],
+  imports: [MatTable, MatTableModule, MatCardModule, MatToolbarModule, HttpClientModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;  // precisa ser um observable pois o metodo que vai popular essa lista retorna um observable
   displayedColumns = ['name', 'category'];
 
   constructor(
     private coursesService: CoursesService
   ) {
-
+    this.courses = this.coursesService.list(); // o metodo retorna um observable
   }
 
   ngOnInit(): void {
-      this.courses = this.coursesService.list();
+
   }
 
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,19 +1,24 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(
-    // private htttpClient: HttpClient
+     private htttpClient: HttpClient
   ) { }
 
   list() {
-    return [
-      { _id: "1", name: "Introdução a machine learning", category: "IA" },
-      { _id: "2", name: "Processamento de linguagem Natural", category: "IA , NLP" }
-    ];
+    return this.htttpClient.get<Course[]>(this.API)
+    .pipe(
+      tap( courses => console.log(courses)) // tap é usado pra manipular melhor a informação que foi recebida
+    );
   }
 }
